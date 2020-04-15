3fd2ccd7185551284a7caef077a83a6cc55db485
raiffas <raiffas@gmail.com>
2020-04-14 21:49:28 -0400

    q4

--- mp3/jenkins/core/src/main/java/jenkins/model/IdStrategy.java
+++ mp3/jenkins/core/src/main/java/jenkins/model/IdStrategy.java
@@ -247,29 +247,34 @@ public abstract class IdStrategy extends AbstractDescribableImpl<IdStrategy> imp
                     } else if (c == '$') {
                         StringBuilder hex = new StringBuilder(4);
                         i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
+                        while (i < chars.length) {
+                        	hex.append(chars[i]);
+                        	i++;
                         }
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
-                        }
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
-                        }
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
-                        }
+//                        i++;
+//                        if (i < chars.length) {
+//                            hex.append(chars[i]);
+//                        } else {
+//                            break;
+//                        }
+//                        i++;
+//                        if (i < chars.length) {
+//                            hex.append(chars[i]);
+//                        } else {
+//                            break;
+//                        }
+//                        i++;
+//                        if (i < chars.length) {
+//                            hex.append(chars[i]);
+//                        } else {
+//                            break;
+//                        }
+//                        i++;
+//                        if (i < chars.length) {
+//                            hex.append(chars[i]);
+//                        } else {
+//                            break;
+//                        }
                         buf.append(Character.valueOf((char)Integer.parseInt(hex.toString(), 16)));
                     }
                 }
